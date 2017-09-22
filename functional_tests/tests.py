18126478b00d77f628052fe0e665d5a12fa2846a
Jeff Tian <jefftian@hcdlearning.com>
2017-09-22 13:52:40 +0800

    refactor templates to use a base template

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -5,6 +5,7 @@ import time
 from django.test import LiveServerTestCase
 
 class NewVisitorTest(LiveServerTestCase):
+
     def setUp(self):
         self.browser = webdriver.Firefox()
         self.browser.implicitly_wait(3)
@@ -67,3 +68,15 @@ class NewVisitorTest(LiveServerTestCase):
         self.assertNotIn('Buy peacock feathers', page_text)
         self.assertIn('Buy milk', page_text)
 
+
+    def test_layout_and_styling(self):
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024, 768)
+
+        inputbox.send_keys('testing\n')
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+                inputbox.location['x'] + inputbox.size['width'] / 2,
+                512,
+                delta=5
+        )
